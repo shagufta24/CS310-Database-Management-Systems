f27d765663ee9531055e601275ca364f6173d804
Shagufta Anjum <shagufta.0201@gmail.com>
2021-10-06 21:19:49 +0530

    Lab 4 update

--- lab_4/assign_4.sql
+++ lab_4/assign_4.sql
@@ -1,11 +1,11 @@
 -- Query 1
-select name from student where gender = 'F' union select name from student where team = 'R';
+select name from student where team = 'R' union select name from student where gender = 'F';
 
 -- Query 2
 select team, count(*) as count from student group by team;
 
 -- Query 3
-select team, avg(math) as avg_math_score from student where gender = 'M' group by team;
+select team, avg(math) as boys_avg_math_score from student where gender = 'M' group by team;
 
 -- Query 4
 select * from student group by team, gender order by team;
