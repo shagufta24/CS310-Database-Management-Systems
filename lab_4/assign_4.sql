f823d1d0c3c0c7ff9d71c9d99eb82d1101258236
Shagufta Anjum <shagufta.0201@gmail.com>
2021-10-06 14:40:07 +0530

    Lab 4

---
+++ lab_4/assign_4.sql
@@ -0,0 +1,14 @@
+-- Query 1
+select name from student where gender = 'F' union select name from student where team = 'R';
+
+-- Query 2
+select team, count(*) as count from student group by team;
+
+-- Query 3
+select team, avg(math) as avg_math_score from student where gender = 'M' group by team;
+
+-- Query 4
+select * from student group by team, gender order by team;
+
+-- Query 5
+select team, avg(sci) from student group by team order by avg(sci) desc limit 2;
